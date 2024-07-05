63e440d1c420df1e82786b0d28925cf8dba3bddb
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-07-05 10:20:45 +0300

    Updated Soft versions #4

--- utils/TestDataProvider.ts
+++ utils/TestDataProvider.ts
@@ -30,9 +30,9 @@ export class TestDataProvider {
         return body;
     }
 
-    static readonly DeviceIdWithWiFi: string = "00:08:B3:30:03:F0"; // Office HUB - with WiFi channel
-    static readonly DeviceIdWithEthernet: string = "00:08:B7:10:08:F4"; // Office HUB - with Ethernet channel
-    static readonly DeviceIdWithGSM: string = "00:08:B7:00:00:08"; // Office HUB - with SimCard channel
+    static readonly DeviceIdWithEthernet: string = "00:08:B3:30:03:F0"; // Office HUB - with WiFi channel
+    static readonly DeviceIdWithGSM: string = "00:08:B7:10:08:F4"; // Office HUB - with Ethernet channel
+    static readonly DeviceIdWithWiFi: string = "00:08:B7:00:00:08"; // Office HUB - with SimCard channel
     static readonly DeviceIdPin: string = "00:08:B7:10:02:04"; // PIN hub
     static readonly DeviceIdTabachkov1: string = "00:08:B7:FE:33:02"; // Tabachkov HUB #1
     static readonly DeviceIdTabachkov2: string = "00:08:B7:99:99:99"; // Tabachkov HUB #2
--- ws/FirmwareVersionConfig.ts
+++ ws/FirmwareVersionConfig.ts
@@ -13,8 +13,8 @@ export interface IFirmwareVersionView  {
 
 export const firmwareVersionConfig = new Map<FirmwareVersionType, IFirmwareVersionConfig>()
     .set(FirmwareVersionType.NEWEST, {
-        versionCode: 2265,
-        url: 'ftp://devfirmware.maks.systems:2221/v2/files/b7/22_65.bin'
+        versionCode: 2266,
+        url: 'http://devfirmware.maks.systems:8080/v2/files/b7/22_66.bin'
     })
     .set(FirmwareVersionType.NEWEST, {
         versionCode: 2264,
