a544d5ad658c70c4ca8647ed23ef35106844e972
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-07-16 18:32:22 +0300

    Skipped test for GSM device

--- specs/e2e/upgrade_device_gsm.spec.ts
+++ specs/e2e/upgrade_device_gsm.spec.ts
@@ -52,7 +52,7 @@ test.describe('[MPX] Automate firmware upgrade/downgrade testing for MPX with Si
 
     });
 
-    test.skip('positive: Success upgrade a device', { tag: '@no-upgrade' }, async ({request}) => {
+    test('positive: Success upgrade a device', { tag: '@upgrade' }, async ({request}) => {
         const TIMEOUT: number = 1200;
         const PAUSE: number = 30000;
         let ERROR: string = "";
