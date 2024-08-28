06a111aedb4462bb0f695df4cbad7a23ecb91f5c
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-08-28 11:27:31 +0300

    unskipped 5 fw test #4

--- api/controllers/HostnameController.ts
+++ api/controllers/HostnameController.ts
@@ -1,6 +1,13 @@
 import { APIRequestContext, APIResponse } from "@playwright/test";
 import { ApiBuilder } from "../builder/ApiBuilder";
 
+// TODO
+type AuthParams = {
+    apiKey: string,
+} | {
+    bearerAuthToken: string
+}
+
 export class HostnameController {
 
     static async getHostname(
@@ -15,5 +22,26 @@ export class HostnameController {
             `/panelServer/discover/api1/dns/${serialNumber}`,
             params
         )
+    }
+
+    static async sendGetRequest(
+        request: APIRequestContext,
+        endpoint: string,
+        authParams: AuthParams,
+        params?: Record<string, string | number | boolean>,
+    ): Promise<APIResponse> {
+        let headers: Record<string, string> = {
+            "Content-Type": "application/json",
+        }
+        if (authParams["apiKey"]) {
+            headers["X-Api-Key"] = authParams["apiKey"];
+        }
+        if (authParams["bearerAuthToken"]) {
+            headers["X-Auth-Token"] = `Bearer ${authParams["bearerAuthToken"]}`;
+        }
+        return await request.get(process.env.API_URL + endpoint, {
+            headers: headers,
+            params: params,
+        });
     }
 }
