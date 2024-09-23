2e10462b1b9f8d68003a34fe657631369a3deafd
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-09-23 12:51:07 +0300

    unskipped 5 fw test #10

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
         ['html', { outputFolder: 'playwright-report', open: 'never' }],
         ['junit', { outputFile: 'test-results/test-results.xml' }],
     ],
-    workers: 3,
+    workers: 1,
     testMatch: /.*\.spec\.ts/,
     fullyParallel: true,
 
