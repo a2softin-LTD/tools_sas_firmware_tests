bc07d287e6f608c12bf48391a6cfb12a9b91f70f
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-12-11 17:00:14 +0200

    updating #1

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 export const DEVICE = process.env.DEVICE_ID || "00:08:B7:10:08:F4";
-export const FIRMWARE_VERSION_URLS = process.env.FIRMWARE_VERSIONS || "http://dev-file.maks.systems:7070/v2/files/b7/22_74.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_73.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_72.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_71.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_75.bin";
+export const FIRMWARE_VERSION_URLS = process.env.FIRMWARE_VERSIONS ||
+    [
+        'http://dev-file.maks.systems:7070/v2/files/b7/22_74.bin',
+        'http://dev-file.maks.systems:7070/v2/files/b7/22_73.bin',
+    ]  .filter(Boolean).join(',')
+// export const FIRMWARE_VERSION_URLS = process.env.FIRMWARE_VERSIONS || "http://dev-file.maks.systems:7070/v2/files/b7/22_74.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_73.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_72.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_71.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_75.bin";
