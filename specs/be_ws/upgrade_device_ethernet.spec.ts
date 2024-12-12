fcb10d5b76b6a63344e73f250188026a6b6c2365
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-12-12 09:44:41 +0200

    updating #21

--- specs/be_ws/upgrade_device_ethernet.spec.ts
+++ specs/be_ws/upgrade_device_ethernet.spec.ts
@@ -65,7 +65,7 @@ test.describe('[MPX] Automate firmware upgrade/downgrade testing for MPX with Et
 
     });
 
-    test('positive: Success upgrade a device', { tag: '@upgrade_' }, async () => {
+    test('positive: Success upgrade a device', async () => {
         // 3. [WSS] Connection and sending necessary commands to the device via web sockets
         try {
             await Timeouts.raceError(async () => {
