b34efc20b9170062bb10baffc6279e8f560d8143
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2025-02-12 16:32:47 +0200

    added reaction tests via WS #19

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ export const SEVERAL_DEVICES_SAS = process.env.SEVERAL_DEVICES_SAS || "00:08:B7:
 export const REACTION_AMOUNT = process.env.REACTION_AMOUNT || "10";
 export const FIRMWARE_VERSION_URLS = process.env.FIRMWARE_VERSIONS ||
         [
-            'http://51.20.115.159:7070/firmware/b7/22_73.bin',
-            'http://51.20.115.159:7070/firmware/b7/22_74.bin',
-            'http://51.20.115.159:7070/firmware/b7/22_75.bin',
+            'http://95.67.118.186:29034/fw/xx_75_08/mpx_ua_dev.ebin',
+            'http://95.67.118.186:29034/fw/xx_76_01/mpx_ua_dev.ebin',
         ]
         .filter(Boolean).join(',');
 
 export const FIRMWARE_VERSION_URLS_ALL_HUBS =
     [
-        'http://51.20.115.159:7070/firmware/b7/22_74.bin',
-        'http://51.20.115.159:7070/firmware/b7/22_75.bin',
+        'http://95.67.118.186:29034/fw/xx_75_08/mpx_ua_dev.ebin',
+        'http://95.67.118.186:29034/fw/xx_76_01/mpx_ua_dev.ebin',
     ]
         .filter(Boolean).join(',')
 // export const FIRMWARE_VERSION_URLS = process.env.FIRMWARE_VERSIONS || "http://dev-file.maks.systems:7070/v2/files/b7/22_74.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_73.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_72.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_71.bin,http://dev-file.maks.systems:7070/v2/files/b7/22_75.bin";
