09d5a6a2154b88f53073c332f4fe4159d1b1a44b
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2025-04-03 23:15:07 +0300

    Added reactions test generator (draft)

--- src/services/Updater.ts
+++ src/services/Updater.ts
@@ -3,18 +3,21 @@ import { IFirmwareVersionView } from "../../ws/FirmwareVersionConfig";
 import { ControlPanelSessionCommands, WsMethod } from "../domain/constants/ws-connection/ws-commands";
 import { ErrorHandler } from "../utils/errors/ErrorHandler";
 import { WsControlHandler } from "./WsControlHandler";
-import { ArmStatesEnum, IControlPanelUpdateBlock } from "../domain/entity/control-session/control-panel-data";
+import {
+    ArmStatesEnum,
+    IControlGroupState, IControlPanelParsedInfo,
+    IControlPanelUpdateBlock
+} from "../domain/entity/control-session/control-panel-data";
+import { PanelParsedInfo } from "../domain/entity/setup-session/PanelParsedInfo";
 
 export class Updater {
     static async currentVersion(wsInstance: WsHandler, serialNumber: number) {
-        const initialData = await wsInstance.createSocket(serialNumber);
+        const initialData: PanelParsedInfo = await wsInstance.createSocket(serialNumber);
         let currentVersion: string = '';
         try {
             currentVersion = initialData['create']['panelSettings']['version'];
-            //wsInstance.close();
         } catch(e) {
             currentVersion = initialData['update']['panelSettings']['version'];
-            //wsInstance.close();
         }
         return currentVersion;
     }
@@ -22,8 +25,10 @@ export class Updater {
     static async update(wsInstance: WsHandler, serialNumber: number, allowedVersionConfig: IFirmwareVersionView) {
         try {
             const initialData = await wsInstance.createSocket(serialNumber);
-            if (initialData['create']['panelSettings']['version'].includes(allowedVersionConfig.config.version))
+            if (initialData['create']['panelSettings']['version'].includes(allowedVersionConfig.config.version)) {
+                console.log('Error 101');
                 throw 1001;
+            }
             // const allowedVersionConfig = getAnotherVersionConfig(initialData['create']['panelSettings']['versionCode'])
             //console.log(`Install the version: ${allowedVersionConfig.versionType}`)
 
@@ -33,126 +38,131 @@ export class Updater {
             wsInstance.close();
         } catch (error) {
             wsInstance.close();
+            console.log(error);
             if (error) {
-                const ERROR = ErrorHandler.handleError(error);
+                const ERROR: string = ErrorHandler.handleError(error);
                 console.log(`versionError ${allowedVersionConfig.versionType}: ${ERROR}`);
                 throw {
                     error: ERROR,
-                    config: allowedVersionConfig
+                    config: allowedVersionConfig,
                 };
             }
         }
     }
 
     static async armPanel(wsInstance: WsControlHandler, serialNumber: number) {
-        const initialData = await wsInstance.createSocket(serialNumber);
-        console.log("armPanel", initialData);
-        const groups = initialData.create?.groups
-        const firstGroupIndex = groups[0].index
-        console.log("firstGroupIndex", firstGroupIndex);
+        const initialData: IControlPanelParsedInfo = await wsInstance.createSocket(serialNumber);
+        const groups: IControlGroupState[] = initialData.create?.groups;
+        const firstGroupIndex: number = groups[0].index;
+
+        console.log("armPanel: ", initialData);
+        console.log("firstGroupIndex: ", firstGroupIndex);
 
         await wsInstance.update({
             subscribeMethod: "update", subscribePart: 'groups',
             validityFunction: (groups: IControlPanelUpdateBlock["groups"]) => {
-                if (!groups?.length) return false
-                const armedGroupExist = Boolean(
+                if (!groups?.length) {
+                    return false;
+                }
+                const armedGroupExist: boolean = Boolean(
                     groups.filter(el => el.states.includes(ArmStatesEnum.ArmedAway))
-                        .some(el => el.index == firstGroupIndex)
+                        .some(el => el.index == firstGroupIndex),
                 )
-
-                return armedGroupExist
+                return armedGroupExist;
             },
             method: ControlPanelSessionCommands.ARM_AWAY,
-            data: {groups: [firstGroupIndex]}
+            data: {groups: [firstGroupIndex]},
         })
         console.log("wsInstance success!!!");
-        wsInstance.close()
+
+        wsInstance.close();
     }
 
     static async disarmPanel(wsInstance: WsControlHandler, serialNumber: number) {
         const initialData = await wsInstance.createSocket(serialNumber);
-        const groups = initialData.create?.groups
+        const groups = initialData.create?.groups;
         const armedGroups = groups
-            .filter(el => el.states.includes(ArmStatesEnum.ArmedAway))
-        if (!armedGroups.length) return true
-        const firstGroupIndex = armedGroups[0].index
+            .filter(el => el.states.includes(ArmStatesEnum.ArmedAway));
+
+        if (!armedGroups.length) {
+            return true;
+        }
+
+        const firstGroupIndex = armedGroups[0].index;
 
         await wsInstance.update({
             subscribeMethod: "update", subscribePart: 'groups',
             validityFunction: (groups: IControlPanelUpdateBlock["groups"]) => {
-                if (!groups?.length) return false
-                const disarmedGroupExist = Boolean(
+                if (!groups?.length) {
+                    return false;
+                }
+                const disarmedGroupExist: boolean = Boolean(
                     groups.filter(el => el.states.includes(ArmStatesEnum.Disarm))
-                        .some(el => el.index == firstGroupIndex)
+                        .some(el => el.index == firstGroupIndex),
                 )
-                return disarmedGroupExist
+                return disarmedGroupExist;
             },
             method: ControlPanelSessionCommands.DISARM,
-            data: {groups: [firstGroupIndex]}
+            data: {groups: [firstGroupIndex]},
         })
-
-        wsInstance.close()
+        wsInstance.close();
     }
 
     static async armGroupsListOfPanel(wsInstance: WsControlHandler, serialNumber: number, groupIndexes: Array<number>) {
-        const initialData = await wsInstance.createSocket(serialNumber);
-        const initialGroups = initialData.create?.groups
-
+        const initialData: IControlPanelParsedInfo = await wsInstance.createSocket(serialNumber);
+        const initialGroups = initialData.create?.groups;
         const armedGroups = initialGroups
-            .filter(el => el.states.some(state => [ArmStatesEnum.ArmedStay, ArmStatesEnum.ArmedAway].includes(state)))
-
-        const notArmedGroupsIndexes = groupIndexes.filter(gi => !armedGroups.some(group => group.index == gi))
+            .filter(el => el.states.some(state => [ArmStatesEnum.ArmedStay, ArmStatesEnum.ArmedAway].includes(state)));
+        const notArmedGroupsIndexes: number[] = groupIndexes.filter(gi => !armedGroups.some(group => group.index == gi));
 
         for (const groupIndex of notArmedGroupsIndexes) {
-
             await wsInstance.update({
                 subscribeMethod: "update", subscribePart: 'groups',
                 validityFunction: (groups: IControlPanelUpdateBlock["groups"]) => {
                     if (!groups?.length) return false
-                    const armedGroupExist = Boolean(
+                    const armedGroupExist: boolean = Boolean(
                         groups.filter(el => el.states.includes(ArmStatesEnum.ArmedAway))
-                            .some(el => el.index == groupIndex)
+                            .some(el => el.index == groupIndex),
                     )
-
-                    return armedGroupExist
+                    return armedGroupExist;
                 },
                 method: ControlPanelSessionCommands.ARM_AWAY,
                 data: {groups: [groupIndex]}
             })
         }
-
         console.log("wsInstance success!!!");
-        wsInstance.close()
+
+        wsInstance.close();
     }
 
     static async disarmAllGroupsOfPanel(wsInstance: WsControlHandler, serialNumber: number) {
         const initialData = await wsInstance.createSocket(serialNumber);
-        const groups = initialData.create?.groups
+        const groups = initialData.create?.groups;
         const armedGroups = groups
-            .filter(el => el.states.some(state => [ArmStatesEnum.ArmedStay, ArmStatesEnum.ArmedAway].includes(state)))
+            .filter(el => el.states.some(state => [ArmStatesEnum.ArmedStay, ArmStatesEnum.ArmedAway].includes(state)));
 
-        if (!armedGroups.length) return true
+        if (!armedGroups.length) {
+            return true;
+        }
 
-        let groupIndex: number
+        let groupIndex: number;
         for (const armedGroup of armedGroups) {
-            groupIndex = armedGroup.index
+            groupIndex = armedGroup.index;
 
             await wsInstance.update({
                 subscribeMethod: "update", subscribePart: 'groups',
                 validityFunction: (groups: IControlPanelUpdateBlock["groups"]) => {
                     if (!groups?.length) return false
-                    const disarmedGroupExist = Boolean(
+                    const disarmedGroupExist: boolean = Boolean(
                         groups.filter(el => el.states.includes(ArmStatesEnum.Disarm))
-                            .some(el => el.index == groupIndex)
+                            .some(el => el.index == groupIndex),
                     )
-                    return disarmedGroupExist
+                    return disarmedGroupExist;
                 },
                 method: ControlPanelSessionCommands.DISARM,
-                data: {groups: [groupIndex]}
+                data: {groups: [groupIndex]},
             })
         }
-
-
-        wsInstance.close()
+        wsInstance.close();
     }
 }
